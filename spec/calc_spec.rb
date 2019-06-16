e0cf04a0fbf597647fb798ce5cf30060de9e0798
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-16 19:18:39 +0900

    subjectを使用してリファクタリングを行う

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,15 +1,12 @@
 require 'calc'
 
-RSpec.describe "A calc" do
-  before do
-    @calc = Calc.new
-  end
+RSpec.describe Calc do
+  subject(:calc) { Calc.new }
   it "given 2 and 3, return 5" do
-    calc = Calc.new
-    expect(@calc.add(2, 3)).to eq(5)
+    expect(calc.add(2, 3)).to eq(5)
   end
   # 三角測量
   it "given 5 and 8, return 13" do
-    expect(@calc.add(5, 8)).to eq(13)
+    expect(calc.add(5, 8)).to eq(13)
   end
 end
