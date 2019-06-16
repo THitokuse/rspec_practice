cde571fc7620a457db6d219efbb4f301130b65bc
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-16 19:23:53 +0900

    letを使用したテストの実装

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Calc do
   it "given 5 and 8, return 13" do
     expect(calc.add(5, 8)).to eq(13)
   end
+
+  # let
+  context "tax 5%" do
+    let(:tax) { 0.05 }
+    it { expect(calc.price(100, tax)).to eq(105) }
+  end
+  context "tax 8%" do
+    let(:tax) { 0.08 }
+    it { expect(calc.price(100, tax)).to eq(108) }
+  end
 end
